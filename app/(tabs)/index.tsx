a086f194cb00211628c585fa2a5cb25aca9f47e9
cuibingbing <cuibingbing@beyongcent.com>
2024-06-28 22:44:38 +0800

    配置用户抽屉

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,63 +1,61 @@
 import { StyleSheet, PanResponder } from "react-native";
-import {useState} from 'react'
+import { useState } from "react";
 import EditScreenInfo from "@/components/EditScreenInfo";
 import { Text, View } from "@/components/Themed";
 import useDrawerStore from "../../Store/drawerState";
-import { TabView,Tab  } from '@rneui/themed';
-
+import { TabView, Tab } from "@rneui/themed";
 
 export default function TabOneScreen() {
   const [index, setIndex] = useState(0);
-  const onOpen = useDrawerStore((state:any)=>state.onOpen),
-  setOpen =  useDrawerStore((state:any)=>state.setOpen);
-  
+  const onOpen = useDrawerStore((state: any) => state.onOpen),
+    setOpen = useDrawerStore((state: any) => state.setOpen);
 
   const _panResponder = PanResponder.create({
     onStartShouldSetPanResponder: (evt, gestureState) => true,
     onMoveShouldSetPanResponder: (evt, gestureState) => true,
     onPanResponderTerminationRequest: (evt, gestureState) => {
-      return onOpen
+      return onOpen;
     },
     onPanResponderGrant: (evt, gestureState) => {
       // setLeft(0);
       // 开始滑动时的处理逻辑
     },
     onPanResponderMove: (evt, gestureState) => {
-        const {dx,vx} = gestureState;
-        if(index == 0 && dx> 10){
-          setOpen(true)
-        }
-        // setLeft(Math.max(gestureState.dx, 0));
-        // // 正在滑动时的处理逻辑
+      const { dx, vx } = gestureState;
+      if ((index == 0 && dx > 10) || vx > 2.5) {
+        setOpen(true);
+      }
+      // setLeft(Math.max(gestureState.dx, 0));
+      // // 正在滑动时的处理逻辑
     },
-    onPanResponderTerminate:(evt,gestureState)=>{
+    onPanResponderTerminate: (evt, gestureState) => {
       // setIndex(1)
     },
     onPanResponderRelease: (evt, gestureState) => {
       // // 滑动结束时的处理逻辑
-      const {dx} = gestureState;
+      const { dx } = gestureState;
       // console.log(index,dx)
-      if(index == 0 && dx  <-10){
-        setIndex(1)
-      }else if(index == 1 && dx >10){
-        setIndex(0)
+      if (index == 0 && dx < -10) {
+        setIndex(1);
+      } else if (index == 1 && dx > 10) {
+        setIndex(0);
       }
-      console.log('end',index)
+      console.log("end", index);
     },
   });
 
   return (
     <View style={styles.container} {..._panResponder.panHandlers}>
-      <Tab containerStyle={{borderBottomWidth:1,borderBottomColor:'#f0f0f0'}}  value={index} onChange={setIndex} dense>
-        <Tab.Item  active = {true}
-            containerStyle={(active)=>({borderColor:active?'blue':'red'})}>Tab</Tab.Item>
-        <Tab.Item 
-            containerStyle={{borderColor:'red'}}
-        >Tab</Tab.Item>
-      </Tab>
-      <Text style={styles.title} >
-        Tab index {index + ""}----
-        Tab onOpen {onOpen + ""}
+      <TabView
+        containerStyle={{ borderBottomWidth: 1, borderBottomColor: "#f0f0f0" }}
+        value={index}
+        onChange={setIndex}
+      >
+        <TabView.Item>TabView</TabView.Item>
+        <TabView.Item>TabView</TabView.Item>
+      </TabView>
+      <Text style={styles.title}>
+        Tab index {index + ""}---- Tab onOpen {onOpen + ""}
       </Text>
       <View
         style={styles.separator}
@@ -80,7 +78,7 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
   },
   separator: {
-    paddingTop:260,
+    paddingTop: 260,
     marginVertical: 30,
     height: 1,
     width: "80%",
--- components/UserDrawer/Drawer.tsx
+++ components/UserDrawer/Drawer.tsx
@@ -1,45 +1,134 @@
-import { useState } from "react";
-import { ScrollView, StyleSheet,TouchableHighlight,TouchableOpacity } from "react-native";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import { FontAwesome } from "@expo/vector-icons";
+import { Feather } from "@expo/vector-icons";
+import {
+  ScrollView,
+  StyleSheet,
+  TouchableHighlight,
+  TouchableOpacity,
+} from "react-native";
 import { Text, View } from "@/components/Themed";
 import { Link } from "expo-router";
 
-
 import UserAvatar from "../user/UserAvatar";
 
+const Menus = [
+  {
+    icon: {
+      component: <AntDesign name="user" size={24} />,
+    },
+    label: "个人空间",
+  },
+  {
+    icon: {
+      component: <FontAwesome name="star-o" size={24} />,
+    },
+    label: "收藏",
+  },
+  {
+    icon: {
+      component: <Feather name="mic" size={24} />,
+    },
+    label: "直播",
+  },
+];
 
 const UserDrawer = () => {
   return (
-    <View style={UserStyles['container']}>
+    <View style={UserStyles["container"]}>
       <TouchableOpacity>
         <UserAvatar></UserAvatar>
       </TouchableOpacity>
-      <TouchableOpacity style={UserStyles['name']}>
-        <View>
-          <Text >John Doe</Text>
-        </View>
+      <TouchableOpacity>
+        <Text style={UserStyles["name"]}>官凉</Text>
       </TouchableOpacity>
-      <TouchableOpacity  style={UserStyles['id']}>
-        <Text >@President</Text>
+      <TouchableOpacity>
+        <Text style={UserStyles["id"]}>@President</Text>
       </TouchableOpacity>
-      
+      <View style={UserStyles["followAboart"]}>
+        <TouchableOpacity
+          style={{ ...UserStyles["following"], paddingRight: 10 }}
+        >
+          <Text style={UserStyles["followAboartCount"]}>0</Text>
+          <Text style={UserStyles["followType"]}>正在关注</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={UserStyles["followers"]}>
+          <Text style={UserStyles["followAboartCount"]}>0</Text>
+          <Text style={UserStyles["followType"]}>关注者</Text>
+        </TouchableOpacity>
+      </View>
       <ScrollView>
-        
+        <View style={UserStyles["menuContainer"]}>
+          {Menus.map((item) => (
+            <TouchableHighlight key={item.label}>
+              <View style={UserStyles["menuItem"]}>
+                {item.icon.component}
+                <Text style={UserStyles["menuLabel"]}>{item.label}</Text>
+              </View>
+            </TouchableHighlight>
+          ))}
+        </View>
       </ScrollView>
+      <TouchableOpacity style={UserStyles["themeChange"]}>
+        <Feather name="sun" size={24} color="black" />
+      </TouchableOpacity>
     </View>
   );
 };
 export default UserDrawer;
 
-const UserStyles = {
-  container:{
-    paddingTop:50,
-    paddingLeft:20,
+const UserStyles = StyleSheet.create({
+  container: {
+    paddingTop: 60,
+    paddingLeft: 30,
+    height: "100%",
   },
   name: {
     paddingTop: 10,
-    fontSize:16
+    fontSize: 16,
+    fontWeight: 500,
   },
-  id: { 
-    paddingTop:2
-},
-};
+  id: {
+    paddingTop: 5,
+    color: "#000000a6",
+  },
+  followAboart: {
+    display: "flex",
+    flexDirection: "row",
+    paddingTop: 10,
+  },
+  following: {
+    display: "flex",
+    flexDirection: "row",
+  },
+  followers: {
+    display: "flex",
+    flexDirection: "row",
+  },
+  followAboartCount: {
+    fontWeight: 500,
+  },
+  followType: {
+    color: "#000000a6",
+    paddingLeft: 4,
+  },
+  menuContainer: {
+    marginTop: 40,
+  },
+  menuItem: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 26,
+  },
+  menuLabel: {
+    fontSize: 20,
+    fontWeight: 500,
+    marginLeft: 25,
+  },
+  themeChange: {
+    position: "absolute",
+    left: 30,
+    bottom: 40,
+  },
+});
